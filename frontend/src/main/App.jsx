3e66c80e24dda6395e7e9d1faecc3e5b05bf4dc3
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-24 23:23:33 -0300

    components created

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react'
+
+// Componentes
+import Logo from "../components/template/Logo.jsx"
+import Nav from "../components/template/Nav.jsx"
+import Main from "../components/template/Main.jsx"
+import Footer from "../components/template/Footer.jsx"
+
+export default props => {
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
+}
