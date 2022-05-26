64f8bea1e724451139e100f74f0a880d562aa49c
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-25 22:18:06 -0300

    nav style updated

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,8 +1,17 @@
-import './Nav.css'
-import React from 'react'
+import "./Nav.css";
+import React from "react";
 
-export default props => 
-    <aside className="menu-area">
-        navegação
-    </aside>
-
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home"></i>
+        Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i>
+        Usuários
+      </a>
+    </nav>
+  </aside>
+);
